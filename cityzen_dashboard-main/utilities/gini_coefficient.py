0cbfb2d3e04393f20ce584e691f5ba0107292844
SKavyaAIFoundry <124775858+SKavyaAIFoundry@users.noreply.github.com>
2023-02-10 10:57:22 +0000

    Delete cityzen_dashboard-main directory
    Signed-off-by: SKavyaAIFoundry <124775858+SKavyaAIFoundry@users.noreply.github.com>

--- cityzen_dashboard-main/utilities/gini_coefficient.py
+++
@@ -1,9 +0,0 @@
-# return the gini coefficient of a given list of values
-def calculate_gini_coefficient(list_of_values):
-    sorted_list = sorted(list_of_values)
-    height, area = 0, 0
-    for value in sorted_list:
-        height += value
-        area += height - value / 2.
-    fair_area = height * len(list_of_values) / 2.
-    return (fair_area - area) / fair_area 
